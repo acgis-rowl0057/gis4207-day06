d5685ecb5b19385521fb6da28eff2ea9c0a5b41d
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-12 17:33:10 -0500

    made a function called setMapDocProps  and all is good.  it works.

--- MatthewRandEmmaY/Mapping01.py
+++ MatthewRandEmmaY/Mapping01.py
@@ -13,11 +13,13 @@ import arcpy
 
 mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
 
-mxd.title = "arcpy.mapping module exercises"
-mxd.author = "Matthew and Emma"
-mxd.credits = "David Viljoen made me do it"
-mxd.summary = "See Description"
-mxd.description = "This document was used for practicing Python coding with the arcpy.mapping module."
-mxd.tags = "arcpy.mapping.python.gis4207"
+def setMapDocProps():
+    mxd.title = "arcpy.mapping module exercises"
+    mxd.author = "Matt and Emma"
+    mxd.credits = "David Viljoen made me do it"
+    mxd.summary = "See Description"
+    mxd.description = "This document was used for practicing Python coding with the arcpy.mapping module."
+    mxd.tags = "arcpy.mapping.python.gis4207"
+    mxd.save()
 
-mxd.save()
+setMapDocProps()
