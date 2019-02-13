78810a4549591f16214771153617b0ded988c875
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-12 21:44:14 -0500

    put my code into a function to clean it up!

--- MatthewRandEmmaY/SaveLayerVisibility.py
+++ MatthewRandEmmaY/SaveLayerVisibility.py
@@ -12,17 +12,17 @@ import arcpy
 import os
 
 mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
-
-
 savePath = r'D:\Semester2\gis4207_Customization_I\day06'
-fileName = "LayerVisibility"
-completeName = os.path.join(savePath,fileName + ".txt")
-file1 = open(completeName, "w")
-for df in arcpy.mapping.ListDataFrames(mxd):
-    for lyr in arcpy.mapping.ListLayers(mxd,"",df):
-        file1.write ("{}\t{}\t{}\n".format(df.name,lyr.name,lyr.visible))
-file1.close()
 
+def writeToText():
+    fileName = "LayerVisibility"
+    completeName = os.path.join(savePath,fileName + ".txt")
+    file1 = open(completeName, "w")
+    for df in arcpy.mapping.ListDataFrames(mxd):
+        for lyr in arcpy.mapping.ListLayers(mxd,"",df):
+            file1.write ("{}\t{}\t{}\n".format(df.name,lyr.name,lyr.visible))
+    file1.close()
 
+writeToText()
 
 
