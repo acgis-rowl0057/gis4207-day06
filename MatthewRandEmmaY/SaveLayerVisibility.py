bdf3c08e7940b00f97db38e09ddabbc39bac5f4e
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-12 21:42:00 -0500

    got it all debugged!!!  proud moment!  yes!!!!!!

--- MatthewRandEmmaY/SaveLayerVisibility.py
+++ MatthewRandEmmaY/SaveLayerVisibility.py
@@ -12,25 +12,17 @@ import arcpy
 import os
 
 mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
+
+
 savePath = r'D:\Semester2\gis4207_Customization_I\day06'
+fileName = "LayerVisibility"
+completeName = os.path.join(savePath,fileName + ".txt")
+file1 = open(completeName, "w")
+for df in arcpy.mapping.ListDataFrames(mxd):
+    for lyr in arcpy.mapping.ListLayers(mxd,"",df):
+        file1.write ("{}\t{}\t{}\n".format(df.name,lyr.name,lyr.visible))
+file1.close()
 
 
-##fileName = "LayerVisibility"
-##completeName = os.path.join(savePath,fileName + ".txt")
-##file1 = open(completeName, "w")
-##file1.write(getDataInfo())
-##file1.close()
 
-def getDataInfo():
-    for df in arcpy.mapping.ListDataFrames(mxd):
-        for lyr in arcpy.mapping.ListLayers(mxd,"",df):
-            return "{}\t{}\t{}".format(df.name,lyr.name,lyr.visible)
 
-def writeToText():
-    fileName = "LayerVisibility"
-    completeName = os.path.join(savePath,fileName + ".txt")
-    file1 = open(completeName, "w")
-    file1.write(getDataInfo())
-    file1.close()
-
-writeToText()
