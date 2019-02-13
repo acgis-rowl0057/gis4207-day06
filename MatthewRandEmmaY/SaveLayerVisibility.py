0fcaa9603e92a466b5f6bf72ec5435da1b4553e8
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-12 20:01:54 -0500

    David can you help me debug my SaveLayerVisibility.py

--- MatthewRandEmmaY/SaveLayerVisibility.py
+++ MatthewRandEmmaY/SaveLayerVisibility.py
@@ -14,20 +14,23 @@ import os
 mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
 savePath = r'D:\Semester2\gis4207_Customization_I\day06'
 
-def main():
+
+##fileName = "LayerVisibility"
+##completeName = os.path.join(savePath,fileName + ".txt")
+##file1 = open(completeName, "w")
+##file1.write(getDataInfo())
+##file1.close()
+
+def getDataInfo():
+    for df in arcpy.mapping.ListDataFrames(mxd):
+        for lyr in arcpy.mapping.ListLayers(mxd,"",df):
+            return "{}\t{}\t{}".format(df.name,lyr.name,lyr.visible)
+
+def writeToText():
     fileName = "LayerVisibility"
     completeName = os.path.join(savePath,fileName + ".txt")
     file1 = open(completeName, "w")
-    file1.write(getDataLayerInfo())
+    file1.write(getDataInfo())
     file1.close()
 
-def getDataLayerInfo():
-    for df in arcpy.mapping.ListDataFrames(mxd):
-        for lyr in arcpy.mapping.ListLayers(mxd,"",df):
-            return df.name, '\t', lyr.name, '\t', lyr.visible
-
-
-
-
-if __name__ == "__main__":
-    main()
+writeToText()
