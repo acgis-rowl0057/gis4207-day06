256c0fcfffee0177925ffa061fe61010a1b3811b
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-12 19:24:06 -0500

    something's not working..... trying to debug this to get is to work.   it will print but when I put it to return and pass it in my write, i get an error saying it should be a string..... arg!!!!

--- MatthewRandEmmaY/SaveLayerVisibility.py
+++ MatthewRandEmmaY/SaveLayerVisibility.py
@@ -8,8 +8,25 @@
 # Copyright:   (c) rowland 2019
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-def main():
-    pass
+import arcpy
+import os
 
-if __name__ == '__main__':
+mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
+
+def main():
+    savePath = r'D:\Semester2\gis4207_Customization_I\day06'
+    fileName = "LayerVisibility"
+    completeName = os.path.join(savePath,fileName + ".txt")
+    file1 = open(completeName, "w")
+    file1.write(getNameVisibility())
+    file1.close()
+
+
+def getNameVisibility():
+    for df in arcpy.mapping.ListDataFrames(mxd):
+        for lyr in arcpy.mapping.ListLayers(mxd,"",df):
+            return df.name, '\t', lyr.name, '\t', lyr.visible
+
+
+if __name__ == "__main__":
     main()
