b12a369d8901c01687c69fa3de7316c2131b0286
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-13 20:47:00 -0500

    loadLayerVisibility is done

--- MatthewRandEmmaY/LoadLayerVisibility.py
+++ MatthewRandEmmaY/LoadLayerVisibility.py
@@ -9,8 +9,36 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-def main():
-    pass
+import arcpy
+import os
 
-if __name__ == '__main__':
-    main()
+mxd = arcpy.mapping.MapDocument("CURRENT")
+text = r'D:\Semester2\gis4207_Customization_I\day06\LayerVisibility.txt'
+
+def changeTextBoolean():
+    with open(text,'r') as file1:
+        fileData = file1.read()
+    fileData = fileData.replace("False", "True")
+    with open(text,'w') as file1:
+        file1.write(fileData)
+changeTextBoolean()
+
+##def getLayerList():
+##    f = open(text,"r")
+##    column = f.readlines()
+##    visibility = []
+##    for x in column:
+##        visibility.append(x.split("\t")[1])
+##    f.close()
+##    print visibility
+##getLayerList()
+
+def turnOnAllLayers():
+    for df in arcpy.mapping.ListDataFrames(mxd):
+        for lyr in arcpy.mapping.ListLayers(mxd,"",df):
+            lyr.visible = True
+    arcpy.RefreshActiveView()
+    arcpy.RefreshTOC()
+
+turnOnAllLayers()
+
