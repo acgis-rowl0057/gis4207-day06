727481fffd2d190ed848c17ca3e4bded63fcfee3
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-15 11:04:59 -0500

    getting mapping04 to return "name of layer"," path","if path endswith ".shp" returns true, else returns false" and describe data type

--- MatthewRandEmmaY/Mapping04.py
+++ MatthewRandEmmaY/Mapping04.py
@@ -8,8 +8,23 @@
 # Copyright:   (c) rowland 2019
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-def main():
-    pass
+import arcpy
 
-if __name__ == '__main__':
-    main()
+mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
+
+def getPath():
+    for df in arcpy.mapping.ListDataFrames(mxd):
+        for lyr in arcpy.mapping.ListLayers(mxd,"",df):
+            if lyr.supports("DATASOURCE"):
+                desc = arcpy.Describe(lyr)
+                print ("{}\t{}\t{}\t{}").format(lyr.name,lyr.dataSource,lyr.dataSource.endswith(".shp"), desc.dataType)
+
+getPath()
+
+
+
+
+
+
+
+
