c58d0b55365065ea0cc7fec84c94e3add3ccf6cf
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-15 22:51:55 -0500

    few bugs to work out...but that will be for tomorrow.  99.9% done the whole assignment!  yes!!!!!

--- MatthewRandEmmaY/Mapping08.py
+++ MatthewRandEmmaY/Mapping08.py
@@ -9,8 +9,44 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-def main():
-    pass
+import arcpy
+import os
 
-if __name__ == '__main__':
-    main()
+
+
+def exportCanadaPDF():
+    mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
+    df = arcpy.mapping.ListDataFrames(mxd)[0]
+    arcpy.mapping.ExportToPDF(mxd, r'D:\Semester2\gis4207_Customization_I\day06\temp\Canada.PDF',df, df_export_width=1600,df_export_height=1200)
+    del mxd
+
+def exportWorldPDF():
+    mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
+    df = arcpy.mapping.ListDataFrames(mxd)[1]
+    arcpy.mapping.ExportToPDF(mxd, r'D:\Semester2\gis4207_Customization_I\day06\temp\World.PDF',df, df_export_width=1600,df_export_height=1200)
+    del mxd
+
+def exportSFPDF():
+    mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
+    df = arcpy.mapping.ListDataFrames(mxd)[2]
+    arcpy.mapping.ExportToPDF(mxd, r'D:\Semester2\gis4207_Customization_I\day06\temp\SanFrancisco.PDF',df, df_export_width=1600,df_export_height=1200)
+    del mxd
+
+def createPDF():
+    pdfPath = r'D:\Semester2\gis4207_Customization_I\day06\temp\AllMaps.PDF'
+    pdfDoc = arcpy.mapping.PDFDocumentCreate(pdfPath)
+    pdfDoc.appendPages(r'D:\Semester2\gis4207_Customization_I\day06\temp\Canada.PDF')
+    pdfDoc.appendPages(r'D:\Semester2\gis4207_Customization_I\day06\temp\World.PDF')
+    pdfDoc.appendPages(r'D:\Semester2\gis4207_Customization_I\day06\temp\SanFrancisco.PDF')
+    pdfDoc.saveAndClose()
+
+
+
+
+
+
+
+
+exportCanadaPDF()
+exportWorldPDF()
+exportSFPDF()
