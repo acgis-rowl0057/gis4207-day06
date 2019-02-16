013e1ad77a2555b465a23a1f5016ec60b3a87167
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-15 21:26:56 -0500

    on a role!!!  yeeehaa!!!!

--- MatthewRandEmmaY/Mapping05.py
+++ MatthewRandEmmaY/Mapping05.py
@@ -8,9 +8,22 @@
 # Copyright:   (c) rowland 2019
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+import arcpy
 
-def main():
-    pass
 
-if __name__ == '__main__':
-    main()
+
+mxd = arcpy.mapping.MapDocument("CURRENT")
+
+def addContinents():
+    df1 = arcpy.mapping.ListDataFrames(mxd,"Canada")[0]
+    addLayer = arcpy.mapping.Layer(r"D:\Semester2\gis4207_Customization_I\Data\World\Continents.lyr")
+    arcpy.mapping.AddLayer(df1, addLayer, "AUTO_ARRANGE")
+
+def addWorldCities():
+    df1 = arcpy.mapping.ListDataFrames(mxd,"Canada")[0]
+    addLayer = arcpy.mapping.Layer(r"D:\Semester2\gis4207_Customization_I\Data\World\World Cities.lyr")
+    arcpy.mapping.AddLayer(df1, addLayer, "AUTO_ARRANGE")
+addContinents()
+addWorldCities()
+arcpy.RefreshActiveView()
+arcpy.RefreshTOC
