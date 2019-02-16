b95a977da1e1f365cb63ce5aa2664b5783c6077d
LAPTOP-JBO2R38U\rowla <rowl0057@algonquinlive.com>
2019-02-15 20:42:39 -0500

    thank you very much David !!

--- MatthewRandEmmaY/Mapping04.py
+++ MatthewRandEmmaY/Mapping04.py
@@ -12,28 +12,35 @@ import arcpy
 
 ##mxd = arcpy.mapping.MapDocument(r'D:\Semester2\gis4207_Customization_I\Data\MappingEx.mxd')
 mxd = arcpy.mapping.MapDocument("CURRENT")
-listOfPoints = []
-def getPath():
+
+def main():
     for df in arcpy.mapping.ListDataFrames(mxd):
-        for lyr in arcpy.mapping.ListLayers(mxd,"",df):
+        for lyr in arcpy.mapping.ListLayers(mxd, "", df):
+            if lyr.isGroupLayer:
+                lyr.visible = True
+                continue
+            if lyr.isRasterLayer:
+                lyr.visible = False
+                continue
             if lyr.supports("DATASOURCE"):
                 desc = arcpy.Describe(lyr)
-                if lyr.dataSource.endswith(".shp"):
-                    if desc.shapeType =="Point":
-                        listOfPoints.append(str(lyr.name))
-                        if lyr.name in listOfPoints:
-                            lyr.visible = True
-                        else:
-                            lyr.visible = False
+                # DV:  Not all feature classes are SHP files
+                #if lyr.dataSource.endswith(".shp"):
+                if desc.shapeType == "Point":
+                    lyr.visible = True
+                else:
+                    lyr.visible = False
 
 ##                        print "this layer is not a point feature class"
 ##                        print lyr.name
 
 ##                        print ("{}\t{}\t{}").format(lyr.name,lyr.dataSource, desc.datatype)
 
-getPath()
-arcpy.RefreshTOC()
-arcpy.RefreshActiveView()
+#getPath()
+    arcpy.RefreshTOC()
+    arcpy.RefreshActiveView()
+
+main()
 
 
 
